142992dcb510c90b107c2040e253058cfd5c1fbe
ascvr <Marufsalim193@gmail.com>
2022-04-03 09:19:13 +0700

    modifikasi autoload & config,menambahkan file form mata kuliah,web,footer,about,header,index,datamatakuliah,stylebuku

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+
+<h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Muhamad Ma'ruf</li>
+ <li>Nama Belakang : Salim</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> kp.Gosali Kec.cikeusal Kab.Serang </li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Cirebon</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>Renang</li>
+ </ul>
+</section>
